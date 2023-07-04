556c0e9ffb8559e3d15db487c515fa3ad18247c2
Paul Carlton <pcarlton@PC892.local>
2023-07-04 13:08:29 -0800

    Adding better summary for the thread locking factory

--- Csharp.Common/Csharp.Common/Utilities/ThreadSafety/ThreadSafeLockingFactory.cs
+++ Csharp.Common/Csharp.Common/Utilities/ThreadSafety/ThreadSafeLockingFactory.cs
@@ -10,9 +10,12 @@ public interface IThreadSafeLockingFactory
 }
 
 /// <summary>
+/// <para>
 /// This class offers a granular approach to interlocking on a complex data structure
 /// by a key, rather than locking on the data structure itself.
+/// </para>
 ///
+/// <para>
 /// A good example would be a list of references to class objects.  The class objects get
 /// updated but not the list itself and you have multiple threads that could be be updating
 /// multiple objects at once.  Instead of locking on the entire list, you want to lock on
@@ -20,19 +23,22 @@ public interface IThreadSafeLockingFactory
 /// object is not recommended nor a best practice.  Instead we have to generate a lock based
 /// on the key identifier of the object in the list (hash code or unique string identifier).
 /// This factory instead allows you to do just that.  By using the locking factory, you can
-/// enter a read state on the list and then a write state on the object key identifier and
+/// enter a read lock on the list and then a write lock on the object key identifier and
 /// operate on the object.  As long as the list doesn't change and is only read from, this
 /// is thread safe.
-/// 
+/// </para>
+///
+/// <para>
 /// Caveat: this is not safe on Dictionaries.  Before operating on complex data structures
-/// it would be wise to consult documentation on the inner workings of "safe" data types in
-/// C# before locking on the type.  Locking on Dictionaries by key may not be inherently thread safe since
-/// internally you're accessing linked lists by hashcode key and can change the linked list
-/// that has the same hashcode but different keys.  You may want to lock on the generated hashcode
-/// value of the object rather than the string key in the dictionary but even then, that might
-/// not be safe because under the hood, the data structure may change the positions of hashed keys
-/// in the linked list for performance reasons.  Check the docs before you try and lock on parts
-/// of a complex built-in data structure.
+/// it would be wise to consult documentation on the inner workings of memory safe data types in
+/// C# before locking on it.  Locking on Dictionaries by key may not be inherently thread safe since
+/// internally you're accessing linked lists by hashcode key so a dictionary key is not truly
+/// unique.  Since keys with the same hashcode get stored in the same list, you may want to lock on
+/// the generated hashcode value of the key rather than key itself in the dictionary but even then,
+/// that might not be safe because under the hood, the data structure may change the positions of
+/// hashed keys in the linked list for performance reasons.  Check the docs before you try and lock
+/// on parts of a complex built-in data structure.
+/// </para>
 /// </summary>
 public class ThreadSafeLockingFactory: IThreadSafeLockingFactory
 {
